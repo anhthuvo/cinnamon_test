8b91831f8d4e50961633990f695e8db2b01dc8ab
Everly <everly@arevolabs.com>
2023-02-06 00:21:45 +0700

    handle case if 10 items shorter than window height

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -6,73 +6,75 @@ import { getUserByNameApi, getUsersApi } from "@/api";
 import Table from "../../components/Table";
 import useDebounce from "@/utils/useDebounce";
 
-const PAGE_SIZE = 30
+const PAGE_SIZE = 10;
 
 export default function Home() {
   const [data, setData] = useState({
     selectNumber: 0,
     users: [] as any,
   });
-  const [searchText, setSearchText] = useState("")
+  const [searchText, setSearchText] = useState("");
   const value = useDebounce(searchText, 1000);
-  const lastUserId = useRef(0)
-  const currentPosition = useRef(0)
+  const lastUserId = useRef(0);
+  const currentPosition = useRef(0);
+  const TableRef = useRef<HTMLDivElement>(null);
 
-  useEffect(
-    () => {
-      if (value) {
-        getUserByNameApi(value).then(res => {
+  useEffect(() => {
+    if (value) {
+      getUserByNameApi(value)
+        .then((res) => {
           setData({
             selectNumber: 0,
             users: [res.data],
           });
         })
-        .catch(error => {
+        .catch((error) => {
           setData({
             selectNumber: 0,
             users: [],
           });
-        })
-      } 
-      else {
-        getUsers(0)
-      }
-    },
-    [value] 
-  );
+        });
+    } else {
+      getUsers(0);
+    }
+  }, [value]);
 
   const lazyLoad = () => {
     if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
-     currentPosition.current = window.pageYOffset
-     getUsers(lastUserId.current)
+      currentPosition.current = window.pageYOffset;
+      getUsers(lastUserId.current);
     }
-   }
+  };
 
-  const getUsers = (userId:number) => {
-    getUsersApi(PAGE_SIZE, userId).then(res => {
-          const newData = userId === 0? [...res.data] : [...data.users, ...res.data]
-          setData({
-            selectNumber: 0,
-            users: newData,
-          })
-          const len = res.data.length
-          lastUserId.current = res.data[len-1].id
-          if (userId !== 0) {
-            window.scrollTo(0, currentPosition.current)
-          }
-      })
-  }
+  const getUsers = (userId: number) => {
+    getUsersApi(PAGE_SIZE, userId).then((res) => {
+      const newData =
+        userId === 0 ? [...res.data] : [...data.users, ...res.data];
+      setData({
+        selectNumber: 0,
+        users: newData,
+      });
+      const len = res.data.length;
+      lastUserId.current = res.data[len - 1].id;
+      window.scrollTo(0, currentPosition.current);
+    });
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', lazyLoad);
-    return () => {
-    window.removeEventListener('scroll', lazyLoad);
+    const tableHeight = TableRef.current?.clientHeight;
+    if (data.users.length > 0 && tableHeight && tableHeight <= window.innerHeight) {
+      getUsers(lastUserId.current);
     }
+
+    window.addEventListener("scroll", lazyLoad);
+    return () => {
+      window.removeEventListener("scroll", lazyLoad);
+    };
   }, [data]);
 
   return (
     <>
-      <main className={styles.warpper}>
+      <main className={styles.warpper} ref={TableRef}>
         <div className={"container " + styles.header}>
           <div className={styles.header_content}>
             <h1 className={typo.title_l}>User manager</h1>
@@ -92,14 +94,11 @@ export default function Home() {
           </div>
         </div>
         <div className={"container"}>
-          {
-            value && data.users.length === 0 ? (
-              <p>Not found</p>
-            ): (
-              <Table data={data} setData={setData} />
-            )
-          }
-          
+          {value && data.users.length === 0 ? (
+            <p>Not found</p>
+          ) : (
+            <Table data={data} setData={setData} />
+          )}
         </div>
         <div className={styles.control_wrapper}>
           <div className={"container"}>
@@ -116,5 +115,3 @@ export default function Home() {
     </>
   );
 }
-
-
